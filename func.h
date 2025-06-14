6a07ab856012dda74dbc097924380af07628733e
ivp0010 <izaiahperry@my.unt.edu>
2025-06-13 20:07:23 -0500

    reconfigured makefile and headed .c files for headers

--- func.h
+++ func.h
@@ -4,93 +4,9 @@
 #include <stdio.h>
 #include "cell.h"
 
-void addRes(rm* manager)
-{
-	manager->numRes = manager->numRes + 1;
-	manager->rStore = (res*)realloc(manager->rStore, manager->numRes * sizeof(res));
-}
-
-void addInd(im* manager)
-{
-	manager->numInd = manager->numInd + 1;
-	manager->iStore = (ind*)realloc(manager->iStore, manager->numInd * sizeof(ind));
-}
-
-void addCom(cm* manager)
-{
-	manager->numCom = manager->numCom + 1;
-	manager->cStore = (com*)realloc(manager->cStore, manager->numCom * sizeof(com));
-}
-
-void splitter(map* city, char filename[], rm* rman, im* iman, cm* cman)
-{
-	FILE* fp = fopen(filename, "r");
-	int ch;
-	int lines = 0;
-	int lineSize = 0;
-	while((ch = fgetc(fp)) != EOF)
-	{	
-		if(ch == '\n')
-		{
-			lines++;
-		}
-		else if(lines == 0 && ch != ',')
-		{
-			lineSize++;
-		}
-
-	}
-
-	city->rows = lines;
-	city->cols = lineSize;
-	fclose(fp);
-	city->m = (cell**)malloc(lines * sizeof(cell*));
-	for(int i = 0; i < lines; i++)
-	{
-		city->m[i] = (cell*)malloc(lineSize * sizeof(cell));
-	}
-	fp = fopen(filename, "r");
-	unsigned int x = 0;
-	unsigned int y = 0;
-	while((ch = fgetc(fp)) != EOF)
-	{
-
-		if(ch == 'r')
-		{		
-			addRes(rman);
-			rman->rStore[rman->numRes - 1].position.x = x;
-			rman->rStore[rman->numRes - 1].position.y = y;
-		}
-		if(ch == 'i')
-		{
-			addInd(iman);
-			iman->iStore[iman->numInd - 1].position.x = x;
-			iman->iStore[iman->numInd - 1].position.y = y;
-		}
-		if(ch == 'c')
-		{
-			addCom(cman);
-			cman->cStore[cman->numCom - 1].position.x = x;
-			cman->cStore[cman->numCom - 1].position.y = y;
-		}
-		if(ch != ',')
-		{
-			if(ch != '\n'){city->m[y][x].type = ch;}
-			x++;
-		}
-		if(ch == '\n')
-		{
-			y++;
-			x = 0;
-		}
-	}
-	
-
-
-	
-
-
-
-}
+void addRes(rm* manager);
+void addInd(im* manager);
+void addCom(cm* manager);
+void splitter(map* city, char filename[], rm* rman, im* iman, cm* cman);
 
 #endif
--- makefile
+++ makefile
@@ -1,6 +1,10 @@
 
-main: main.o
-	-gcc main.o -o ccity
+main: main.o func.o graph.o
+	-gcc main.o func.o graph.o -o ccity
 main.o: main.c cell.h func.h graph.h
 	-gcc -c main.c
+func.o: func.c cell.h func.h graph.h
+	-gcc -c func.c
+graph.o: graph.c graph.h func.h cell.h
+	-gcc -c graph.c
 
